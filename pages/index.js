bfe0649e9999311968663344e3b7a27fb050aa30
Masaki Yamamoto <masaki.yamamoto@sitecore.com>
2022-06-17 17:09:45 +0900

    Add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'; // 新たにインポート
 
-export default function Home() {
+// パラメーターに allPostsData を追加
+export default function Home({ allPostsData }) {
   return (
     // Layout に home であることを明示的に指定
     <Layout home>
@@ -16,6 +18,31 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      {/* ブログの目次を表示する処理を追加 */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+// 静的生成時にデータを取得する処理を追加
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
